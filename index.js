9b4b854f9ea00c2dd5c293184444ba42c9549cac
searleser97 <gabrielsanv97@gmail.com>
2024-05-11 13:38:14 -0700

    add telegram bot functionality

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const player = require("play-sound")((opts = {}));
 const axios = require("axios");
+const { Telegraf } = require("telegraf");
 
 const sleep = (seconds) =>
   new Promise((resolve) => setTimeout(resolve, Math.ceil(seconds * 1000)));
 
 
 async function main() {
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+  bot.start((ctx) => ctx.reply("Welcome!"));
   const statesOfInterest = ["WA", "OR"];
   let attempt = 1;
   while (true) {
@@ -18,9 +21,13 @@ async function main() {
       console.log(locationsArray);
       for (const location of locationsArray) {
         if (((location?.timeSlots?.length) ?? 0) > 0) {
-          player.play("alarm.wav", function (err) {
-            if (err) throw err;
-          });
+          bot.telegram.sendMessage(
+            process.env.CHAT_ID,
+            `Appointment available in ${state} at ${location.name}`
+          );
+          // player.play("alarm.wav", function (err) {
+          //   if (err) throw err;
+          // });
         }
       }
       await sleep(10);
