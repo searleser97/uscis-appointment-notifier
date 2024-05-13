eec4cece3d865901307cd5f6e78985839186fe4e
searleser97 <gabrielsanv97@gmail.com>
2024-05-12 23:35:05 -0700

    allow to search for specific service centers

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const sleep = (seconds) =>
 async function main() {
   const bot = new Telegraf(process.env.BOT_TOKEN);
   bot.start((ctx) => ctx.reply("Welcome!"));
+  bot.launch();
+  process.once('SIGINT', () => bot.stop('SIGINT'))
+  process.once('SIGTERM', () => bot.stop('SIGTERM'))
   const statesOfInterest = ["WA", "OR"];
+  // populate this array after you have already made an appointment somewhere decent so that you can now be more strict on the location you want to find
+  const serviceCentersOfInterest = [ { state: "WA", serviceCenterId: "XSE" }, { state: "OR", serviceCenterId: "XPL" }];
   let attempt = 1;
   while (true) {
     console.log("attempt number", attempt);
@@ -18,19 +23,23 @@ async function main() {
         `https://my.uscis.gov/appointmentscheduler-appointment/field-offices/state/${state}`
       );
       const locationsArray = response?.data ?? [];
-      console.log(locationsArray);
       for (const location of locationsArray) {
         if (((location?.timeSlots?.length) ?? 0) > 0) {
-          bot.telegram.sendMessage(
-            process.env.CHAT_ID,
-            `Appointment available in ${state} at ${location.name}`
-          );
-          // player.play("alarm.wav", function (err) {
-          //   if (err) throw err;
-          // });
+          console.log(JSON.stringify(locationsArray, null, 2));
+          if (serviceCentersOfInterest.length === 0 || serviceCentersOfInterest.some((sc) => sc.state === state && sc.serviceCenterId === location.assignedServiceCenter)) {
+            for (const timeSlot of location.timeSlots) {
+              bot.telegram.sendMessage(
+                process.env.CHAT_ID,
+                `Appointment available in ${state} at ${location.description} on ${timeSlot.date}`
+              );
+            }
+            player.play("alarm.wav", function (err) {
+              if (err) throw err;
+            });
+          }
         }
       }
-      await sleep(10);
+      await sleep(3);
     }
     attempt++;
     await sleep(60 * 5);
